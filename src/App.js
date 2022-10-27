ce692e764352d20d24794d7424d1b2f6f52e4d74
Caleb Hill <chill389cc@gmail.com>
2022-10-27 17:15:05 -0600

    Changes by ethanborsky

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import NewPost from './pages/NewPost'
+// import RedditFeed from './pages/RedditFeed'
+import NoPage from './pages/NoPage'
 
-function App() {
+/*
+          <Route
+            path="RedditFeed"
+            element={<RedditFeed />}
+          />
+
+          maybe this in the future...
+ */
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="newpost" element={<NewPost />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
